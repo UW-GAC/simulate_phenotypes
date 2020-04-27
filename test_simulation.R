07c17300f430e2e34b8c6a94ca4ff5319eb1e752
Stephanie M. Gogarten <sdmorris@uw.edu>
2020-04-27 19:34:57 +0000

    additional tests

--- test_simulation.R
+++ test_simulation.R
@@ -24,3 +24,32 @@ outcome <- outcome_from_covMat(covMat, varComp)
 dat <- data.frame(y=outcome, row.names=rownames(covMat))
 nullmod <- fitNullModel(dat, outcome="y", cov.mat=covMat)
 nullmod$varComp
+
+
+## test with dense vs sparse matrix
+dense <- GWASTools::getobj("/sbgenomics/project-files/1KG_pcrelate_dense_Matrix.RData")
+sparse <- GWASTools::getobj("/sbgenomics/project-files/1KG_pcrelate_Matrix.RData")
+
+lapply(list(dense=dense, sparse=sparse), function(covMat) {
+  varComp <- c(40, 100)
+  set.seed(100)
+  outcome <- outcome_from_covMat(covMat, varComp)
+
+  dat <- data.frame(y=outcome, row.names=rownames(covMat))
+  nullmod <- fitNullModel(dat, outcome="y", cov.mat=covMat)
+  nullmod$varComp
+})
+
+
+## test with TOPMed freeze 8
+covMat <- GWASTools::getobj("/sbgenomics/project-files/pcrelate_kinshipMatrix_sparseDeg4_v2.RData")
+
+## svd gives "Cholmod error 'problem too large'"
+cov25k <- covMat[1:25000,1:25000]
+
+varComp <- c(40, 100)
+outcome <- outcome_from_covMat(cov25k, varComp)
+
+dat <- data.frame(y=outcome, row.names=rownames(covMat))
+nullmod <- fitNullModel(dat, outcome="y", cov.mat=covMat)
+nullmod$varComp
