2076114bf9a0af2f3164b440296fb102716f536e
Stephanie M. Gogarten <sdmorris@uw.edu>
2020-08-31 22:24:57 -0700

    script to calculate allele frequency for specific samples

---
+++ allele_freq.R
@@ -0,0 +1,27 @@
+library(argparser)
+library(SeqVarTools)
+library(TopmedPipeline)
+
+argp <- arg_parser("allele frequency")
+argp <- add_argument(argp, "gds.file", help="GDS file")
+argp <- add_argument(argp, "out.file", help="out file")
+argp <- add_argument(argp, "--sample.file", help="sample file")
+argp <- add_argument(argp, "--cpu", help="Number of CPUs to utilize for parallel processing", default=1)
+argv <- parse_args(argp)
+
+gds <- seqOpen(argv$gds.file)
+
+if (!is.na(argv$sample.file)) {
+    samp <- getobj(argv$sample.file)
+    seqSetFilter(gds, sample.id=samp)
+}
+
+ref.freq <- seqAlleleFreq(gds, parallel=argv$cpu)
+alt.freq <- 1-ref.freq
+
+var.info <- variantInfo(gds, alleles=FALSE)
+
+res <- cbind(var.info, alt.freq)
+save(res, file=argv$out.file)
+
+seqClose(gds)
