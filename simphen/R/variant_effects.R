7b31c91e9d2d7f6c6dc78d04cb58cc428dd1c469
Stephanie M. Gogarten <sdmorris@uw.edu>
2024-05-20 15:47:54 -0700

    new version of GENESIS changed null model object structure

--- simphen/R/variant_effects.R
+++ simphen/R/variant_effects.R
@@ -16,7 +16,7 @@ variant_effect <- function(G, h2, beta, varComp) {
     if (!missing(h2) & !missing(beta)) {
          stop("only one of h2 and beta may be specified")
     }
-    
+
     if (missing(beta)) {
         htot2 <- (varComp[1] + h2*varComp[2])/sum(varComp)
         beta <- sqrt((h2*varComp[2])/((1-htot2)*var(G)))
@@ -26,7 +26,7 @@ variant_effect <- function(G, h2, beta, varComp) {
     } else {
         stop("h2 or beta must be specified")
     }
-    
+
     Gbeta <- G * beta
     return(list(h2=h2, beta=beta, Gbeta=Gbeta))
 }
@@ -49,7 +49,7 @@ heritability <- function(G, beta, varComp) {
 
 
 #' Calculate power
-#' 
+#'
 #' @param N number of samples
 #' @param h2 heritability
 #' @param pval p-value threshold for significance
@@ -107,11 +107,11 @@ variant_assoc <- function(G, h2=NULL, beta=NULL, varComp, dat, outcome, cov.mat,
     if (!requireNamespace("GENESIS")) {
         stop("must install GENESIS to use variant_assoc")
     }
-    
+
     if (is.null(h2) & is.null(beta)) {
         stop("one of h2 or beta must be specified")
     }
-    
+
     if (is.null(strata)) {
         strata <- rownames(G)
     }
@@ -123,22 +123,22 @@ variant_assoc <- function(G, h2=NULL, beta=NULL, varComp, dat, outcome, cov.mat,
     if (!(setequal(names(strata), names(h2)) | setequal(names(strata), names(beta)))) {
         stop("h2 or beta must match strata")
     }
-    
+
     dat$sample.id <- as.character(dat$sample.id)
     rownames(dat) <- dat$sample.id
-    
+
     if (nrow(dat) < nrow(cov.mat)) {
         sel <- rownames(cov.mat) %in% dat$sample.id
         cov.mat <- cov.mat[sel,sel]
     }
-    
+
     variant.id <- colnames(G)
     if (is.null(variant.id)) variant.id <- 1:ncol(G)
     res <- list()
     for (grp in names(strata)) {
         samp <- as.character(strata[[grp]])
         N <- length(samp)
-        
+
         G.grp <- G[as.character(samp),,drop=FALSE]
         freq <- 0.5*colMeans(G.grp, na.rm=TRUE)
         res.grp <- list()
@@ -148,20 +148,20 @@ variant_assoc <- function(G, h2=NULL, beta=NULL, varComp, dat, outcome, cov.mat,
             } else {
                 eff <- variant_effect(G=as.vector(G.grp[,i]), h2=h2[[grp]], varComp=varComp)
             }
-            
+
             tmp <- Biobase::pData(dat)
             tmp[samp, outcome] <- tmp[samp, outcome] + eff$Gbeta
             Biobase::pData(dat) <- tmp
             res.grp[[i]] <- list(group=grp, N=N, beta=eff$beta, h2=eff$h2, power=power(N, eff$h2, pval=power.signif))
         }
         res.grp <- as.data.frame(data.table::rbindlist(res.grp))
-        
-        nullmod <- GENESIS::fitNullModel(dat, outcome=outcome, covars=covars, cov.mat=cov.mat, 
+
+        nullmod <- GENESIS::fitNullModel(dat, outcome=outcome, covars=covars, cov.mat=cov.mat,
                                          sample.id=samp, start=varComp, verbose=FALSE)
-        assoc <- GENESIS:::testGenoSingleVar(nullmod, G[as.character(nullmod$sample.id),])
+        assoc <- GENESIS:::testGenoSingleVar(nullmod, G[as.character(nullmod$fit$sample.id),])
         res[[grp]] <- cbind(variant.id, res.grp, freq, assoc, stringsAsFactors=FALSE)
     }
-    
+
     if (length(strata) > 1) {
         grp <- "pooled"
         samp <- as.character(unlist(strata, use.names=FALSE))
@@ -180,13 +180,13 @@ variant_assoc <- function(G, h2=NULL, beta=NULL, varComp, dat, outcome, cov.mat,
         }
         power.pooled <- power(N, h2.pooled, pval=power.signif)
         res.grp <- list(group=grp, N=N, beta=beta.pooled, h2=h2.pooled, power=power.pooled)
-        
-        nullmod <- GENESIS::fitNullModel(dat, outcome=outcome, covars=covars, cov.mat=cov.mat, 
+
+        nullmod <- GENESIS::fitNullModel(dat, outcome=outcome, covars=covars, cov.mat=cov.mat,
                                          sample.id=samp, start=varComp, verbose=FALSE)
-        assoc <- GENESIS:::testGenoSingleVar(nullmod, G[as.character(nullmod$sample.id),])
+        assoc <- GENESIS:::testGenoSingleVar(nullmod, G[as.character(nullmod$fit$sample.id),])
         res[[grp]] <- cbind(variant.id, res.grp, freq, assoc, stringsAsFactors=FALSE)
     }
-    
+
     res <- as.data.frame(data.table::rbindlist(res))
     return(res)
 }
@@ -194,7 +194,7 @@ variant_assoc <- function(G, h2=NULL, beta=NULL, varComp, dat, outcome, cov.mat,
 
 
 #' Return genotypes for set of samples and variants
-#' 
+#'
 #' @param gdsobj \code{\link[SeqArray]{SeqVarGDSClass}} object
 #' @param variant.sel indices of variants to return
 #' @param variant.id ids of variants to return
@@ -205,17 +205,17 @@ variant_genotypes <- function(gdsobj, variant.sel, variant.id, sample.id=NULL) {
     if (!requireNamespace("SeqArray")) {
         stop("must install SeqArray and GENESIS to use variant_genotypes")
     }
-    
+
     if (missing(variant.sel) & missing(variant.id)) {
         stop("one of variant.sel or variant.id must be specified")
     }
-    
+
     if (!missing(variant.sel)) {
         SeqArray::seqSetFilter(gdsobj, variant.sel=variant.sel, sample.id=sample.id, verbose=FALSE)
     } else {
         SeqArray::seqSetFilter(gdsobj, variant.id=variant.id, sample.id=sample.id, verbose=FALSE)
     }
-    
+
     geno <- SeqArray::seqGetData(gdsobj, "$dosage_alt")
     rownames(geno) <- SeqArray::seqGetData(gdsobj, "sample.id")
     colnames(geno) <- SeqArray::seqGetData(gdsobj, "variant.id")
